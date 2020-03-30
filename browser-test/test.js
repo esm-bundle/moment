9e36599544a4dc3fca730c779ba456bd9bbc303d
Justin McMurdie <mcmurdiej@gmail.com>
2020-03-30 13:57:09 -0600

    working unit tests

--- browser-test/test.js
+++ browser-test/test.js
@@ -1,3 +1,4 @@
+import moment from "../esm/index.js";
 const timestamp = 636188400000;
 describe("@esm-bundle/moment", () => {
   it("can load the ESM bundle", () => {
@@ -5,12 +6,19 @@ describe("@esm-bundle/moment", () => {
   });
 
   it(`has default as a function`, async () => {
-    const moment = await import("/base/esm/index.js");
-    const momentDate = moment.default(timestamp);
-    momentDate.format("MM DD YYYY");
+    const momentDate = moment(timestamp);
+    expect(momentDate.format("MM")).toEqual("02");
   });
 
-  it("can load the System.register bundle", () => {
-    return System.import("/base/system/index.js");
+  describe(`systemjs`, () => {
+    it("can load the System.register bundle", () => {
+      return System.import("/base/system/index.js");
+    });
+
+    it(`has default as a function`, async () => {
+      const moment = await System.import("/base/system/index.js");
+      const momentDate = moment.default(timestamp);
+      expect(momentDate.format("MM")).toEqual("02");
+    });
   });
 });
